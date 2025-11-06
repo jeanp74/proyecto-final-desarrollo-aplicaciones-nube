99903af488aa76b599c692af3eca9ebfd509e66c
jeanp74 <jeanpaulsolano74@gmail.com>
2025-11-06 16:07:53 -0500

    SEPARACION DEL FRONT Y EL BACK 2025-11-06

--- patients-api/src/db.js
+++
@@ -1,8 +0,0 @@
-// Conexión a PostgreSQL (Azure PG exige TLS)
-import pg from "pg";
-const { Pool } = pg;
-
-export const pool = new Pool({
-  connectionString: process.env.PATIENTS_DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-});
